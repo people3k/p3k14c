a565bbda432092c0962e80d4ad37f2e5458f5378
Kyle Bocinsky <bocinsky@gmail.com>
2021-03-31 13:24:36 -0600

    starting readme

---
+++ README.md
@@ -0,0 +1,47 @@
+
+# p3k14c
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This is the submission release for the P3K14C data publication:
+
+> Darcy Bird [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-3466-6284),
+Lux Miranda [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0002-3753-7405),
+Marc Vander Linden [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0002-0120-7754),
+Robinson, Erick [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0002-0789-3724),
+R. Kyle Bocinsky [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-1862-3428),
+Chris Nicholson [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-3212-2662),
+José M. Capriles [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-6046-0939),
+Judson Byrd Finley [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-0233-8630),
+Eugenia M. Gayo [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-0746-0512),
+Adolfo Gil [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-5718-8866),
+Jade d’Alpoim Guedes [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0002-2627-7863),
+Julie A. Hoggarth [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-8612-8846),
+Andrea Kay [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-8285-1893),
+Emma Loftus [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-4442-8511),
+Umberto Lombardo [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0002-0001-4870),
+Madeline Mackie [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-0769-2348),
+Alessio Palmisano [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0003-0758-5032),
+Steinar Solheim [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-8293-8147),
+Jacob Freeman [![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-7402-8450),
+Robert L. Kelly[![](https://orcid.org/sites/default/files/images/orcid_16x16.png)](https://orcid.org/0000-0001-9737-0152),
+> ( **In review**). p3k14c: A synthetic global database of archaeological radiocarbon dates.
+
+## Installation
+
+You can install the released version of p3k14c from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("p3k14c")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(p3k14c)
+## basic example code
+```
+
---
+++ vignettes/articles/p3k14c_scripts/Marcp3k_risk.R
@@ -0,0 +1,195 @@
+#### sensitivity (including kde) analysis for SciPaper####
+library(p3k14c)
+library(ggplot2)
+
+# Spatial windows for risk analysis
+windows <-
+  list(
+    
+    # Global land area (EPSG 8857)
+    `World` = p3k14c::world,
+    
+    # Northwestern Europe in Albers Equal Area Conic (ESRI 102013)
+    `Northwestern Europe` = p3k14c::nw_europe,
+    
+    # Contiguous USA in Albers Equal Area Conic (ESRI 102003)
+    `United States` = p3k14c::conus
+    
+  )
+
+# Radiocarbon date data
+p3k14c_data <-
+  "data/raw_data/P3k14C_scrubbed_fuzzed.csv" %>%
+  here::here() %>%
+  readr::read_csv(col_types = 
+                    readr::cols(
+                      Age = readr::col_double(),
+                      Error = readr::col_double(),
+                      Long = readr::col_double(),
+                      Lat = readr::col_double(),
+                      .default = readr::col_character()
+                    )) %>%
+  
+  tidyr::drop_na(Long, Lat) %>%
+  dplyr::count(SiteID,
+               SiteName,
+               Continent,
+               Long,
+               Lat,
+               name = "Dates") %>%
+  sf::st_as_sf(
+    coords = c("Long", "Lat"),
+    crs = "EPSG:4326",
+    remove = FALSE
+  ) %>%
+  # Transform to equal earth projection (EPSG 8857)
+  sf::st_transform("EPSG:8857")
+
+# Count dates/sites
+p3k14c_data %>%
+  sf::st_drop_geometry() %>%
+  dplyr::group_by(Continent) %>%
+  dplyr::summarise(Dates = sum(Dates, na.rm = TRUE),
+                   Sites = dplyr::n()) %>%
+  na.omit() %>%
+  dplyr::arrange(dplyr::desc(Dates))
+
+# Create a Point Pattern ("ppp") object
+p3k14c_data_ppp <-
+  p3k14c_data %>%
+  # Cast to a Spatial Points object
+  sf::as_Spatial() %>%
+  # Cast to a Point Pattern object
+  maptools::as.ppp.SpatialPointsDataFrame() %>%
+  # Window to extend KDE around globe
+  window(
+    windows$World %>%
+      sf::st_bbox() %>%
+      sf::st_as_sfc() %>%
+      sf::as_Spatial() %>%
+      maptools::as.owin.SpatialPolygons()
+  )
+
+
+# Create a KDE of the sites
+p3k14c_kde_sites <-
+  sparr::bivariate.density(pp = p3k14c_data_ppp, 
+                           h0 = 200000, 
+                           adapt = FALSE, 
+                           edge = "none",
+                           resolution = 4000)
+
+# Create a KDE of the sites, 
+# weighted by the number of dates at each site
+p3k14c_kde_dates <-
+  sparr::bivariate.density(pp = p3k14c_data_ppp, 
+                           h0 = 200000, 
+                           adapt = FALSE, 
+                           edge = "none",
+                           resolution = 4000,
+                           weights = p3k14c_data_ppp$marks$Dates)
+
+# Calculate the spatial relative risk/density ratio
+p3k14c_risk <-
+  sparr::risk(p3k14c_kde_sites,
+              p3k14c_kde_dates,
+              tolerate = TRUE)
+
+
+normalize_raster <- 
+  function(x){
+    # This simply re-normalizes so the values in the area of interest equal 1
+    raster::values(x) <- 
+      raster::values(x) %>% 
+      {
+        . / sum(., na.rm = TRUE)
+      }
+    x
+  }
+
+plot_kde <- 
+  function(global_kde, windows){
+    windows %>%
+      purrr::map(
+        ~(
+          global_kde %>%
+            as_raster.bivden(crs = "EPSG:8857",
+                             crop = .x) %>%
+            raster::projectRaster(crs = 
+                                    .x %>%
+                                    sf::st_crs() %>%
+                                    as("CRS")) %>%
+            normalize_raster() %>%
+            raster::trim() %>%
+            raster::as.data.frame(xy = TRUE) %>%
+            na.omit() %>%
+            {
+              ggplot() +
+                geom_tile(data = .,
+                          mapping = aes(x = x, 
+                                        y = y, 
+                                        fill = layer)) +
+                geom_sf(data = .x,
+                        fill = "transparent",
+                        color = "black") +
+                scale_fill_viridis_c(option = "C") +
+                ggplot2::theme_minimal()
+            }
+          
+        )
+      )
+  }
+
+plot_risk <- 
+  function(global_risk, windows){
+    windows %>%
+      purrr::map(
+        ~(
+          global_risk %>%
+            as_raster.rrs(crs = "EPSG:8857",
+                          crop = .x) %>%
+            raster::projectRaster(crs = 
+                                    .x %>%
+                                    sf::st_crs() %>%
+                                    as("CRS")) %>%
+            raster::trim() %>%
+            raster::as.data.frame(xy = TRUE) %>%
+            na.omit() %>%
+            {
+              ggplot() +
+                geom_tile(data = .,
+                          mapping = aes(x = x, 
+                                        y = y, 
+                                        fill = rr)) +
+                geom_sf(data = .x,
+                        fill = "transparent",
+                        color = "black") +
+                geom_contour(data = .,
+                             mapping = aes(x = x, 
+                                           y = y,
+                                           z = p),
+                             breaks = 0.05,
+                             colour = "white") +
+                scale_fill_viridis_c(option = "C",
+                                     limits = c(-0.5, 0.5)) +
+                ggplot2::theme_minimal()
+            }
+          
+        )
+      )
+  }
+
+# Generate the plots
+p3k14c_plots <- 
+  list(
+    sites = 
+      plot_kde(global_kde = p3k14c_kde_sites,
+               windows = windows),
+    dates = 
+      plot_kde(global_kde = p3k14c_kde_dates,
+               windows = windows),
+    risk = 
+      plot_risk(global_risk = p3k14c_risk,
+                windows = windows)
+  ) %>%
+  purrr::transpose()
